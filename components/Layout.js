80ee3baea82290a10f6647cbc75d1eb672860456
José <jose14aular@gmail.com>
2021-03-23 10:07:00 -0400

    added a class name in main tag

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Footer from "@/components/Footer";
 export default function Layout({ children }) {
   return (
     <>
-      <main className="px-4 mx-auto my-14 max-w-7xl sm:px-6 lg:px-8">
+      <main className="min-h-screen px-4 mx-auto my-14 max-w-7xl sm:px-6 lg:px-8">
         {children}
       </main>
 
