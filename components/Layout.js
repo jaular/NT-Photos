2a2203e6826e8b67c8f5a8e48b84a8d8d425d6b6
José <jose14aular@gmail.com>
2021-04-02 10:32:34 -0400

    minor changes

--- components/Layout.js
+++ components/Layout.js
@@ -2,12 +2,12 @@ import Footer from "@/components/Footer";
 
 export default function Layout({ children }) {
   return (
-    <>
-      <main className="min-h-screen px-4 mx-auto my-14 max-w-7xl sm:px-6 lg:px-8">
+    <div className="flex flex-col min-h-screen">
+      <main className="container flex-grow px-4 mx-auto my-14 max-w-7xl sm:px-6 lg:px-8">
         {children}
       </main>
 
       <Footer />
-    </>
+    </div>
   );
 }
