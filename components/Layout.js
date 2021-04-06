7f10e0fc6b7409d24bee1f1784f9ad14800f3f1f
José <jose14aular@gmail.com>
2021-04-06 14:27:32 -0400

    styles changed

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Footer from "@/components/Footer";
 export default function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <main className="container flex-grow px-4 mx-auto my-14 max-w-7xl sm:px-6 lg:px-8">
+      <main className="container flex-grow px-4 mx-auto mt-8 md:mt-14 mb-14 max-w-7xl sm:px-6 lg:px-8">
         {children}
       </main>
 
--- pages/photos/[id].js
+++ pages/photos/[id].js
@@ -22,8 +22,8 @@ export default function Photos({ photo }) {
 
       <section>
         {Object.entries(photo).length !== 0 ? (
-          <div className="container flex flex-col items-center mx-auto md:flex-row">
-            <div className="w-full mb-10 lg:max-w-lg lg:w-full md:w-1/2 md:mb-0">
+          <div className="flex flex-col w-full md:flex-row">
+            <div className="w-full md:w-1/2">
               <div className="rounded-lg bg-contrast-higher aspect-w-14 aspect-h-16">
                 <Image
                   className="object-cover object-center rounded-lg"
@@ -33,33 +33,29 @@ export default function Photos({ photo }) {
                 />
               </div>
             </div>
-            <div className="flex flex-col items-center text-left lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 md:items-start">
+            <div className="flex flex-col items-center justify-center w-full pt-10 md:w-1/2 lg:pl-24 md:pl-16 md:items-start">
               <User
                 name={photo.user.name}
                 profile={photo.user.profile_image.large}
                 link={photo.user.links.html}
                 username={photo.user.username}
               />
-
               <div className="flex justify-center">
-                <div className="inline-flex">
-                  <button
-                    onClick={() => router.back()}
-                    className="items-center justify-center px-4 py-2 text-sm font-medium bg-green-600 border border-transparent rounded-md text-contrast-lower sm:text-base hover:bg-green-500 focus:outline-none focus:ring-1 focus:ring-contrast-lower"
-                  >
-                    Back
-                  </button>
-                </div>
-                <div className="inline-flex ml-3">
-                  <a
-                    href={photo.links.html}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="items-center justify-center px-4 py-2 text-sm font-medium border border-transparent rounded-md text-contrast-lower focus:outline-none focus:ring-1 focus:ring-green-500 bg-contrast-higher sm:text-base hover:bg-contrast-high"
-                  >
-                    Unsplash
-                  </a>
-                </div>
+                <button
+                  onClick={() => router.back()}
+                  className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium bg-green-600 border border-transparent rounded-md text-contrast-lower sm:text-base hover:bg-green-500 focus:outline-none focus:ring-1 focus:ring-contrast-lower"
+                >
+                  Back
+                </button>
+
+                <a
+                  href={photo.links.html}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-flex items-center justify-center px-4 py-2 ml-3 text-sm font-medium border border-transparent rounded-md text-contrast-lower focus:outline-none focus:ring-1 focus:ring-green-500 bg-contrast-higher sm:text-base hover:bg-contrast-high"
+                >
+                  Unsplash
+                </a>
               </div>
             </div>
           </div>
